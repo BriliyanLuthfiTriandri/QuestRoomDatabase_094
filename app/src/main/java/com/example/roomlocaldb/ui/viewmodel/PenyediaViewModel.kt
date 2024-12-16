a74aee8dfc725362d55595da4af5b7b5a5f145d8
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-16 19:58:13 +0700

    Menambahkan PenyediaViewModel untuk menyediakan berbagai ViewModel dan menambahkan fugsi krsApp untuk mendapatkan objek krsApp dari parameter yang diberikan

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.roomlocaldb.ui.viewmodel
 
 import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -15,8 +16,27 @@ object PenyediaViewModel{
                 krsApp().containerApp.repositoryMhs
             )
         }
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
     }
 }
+
+
 
 fun CreationExtras.krsApp(): KrsApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
