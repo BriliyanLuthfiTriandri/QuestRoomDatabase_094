1e4fdd5d3ef34a5081854855a6f866ae154c407f
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-16 19:21:56 +0700

    Membuat DetailMhsViewModel untuk mengambil dan menampilkan detail mahasiswa berdasarkan NIM

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/DetailMhsViewModel.kt
@@ -20,10 +20,38 @@ class DetailMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 ) : ViewModel() {
-    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+    private val _nim : String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
 
-    }
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState (
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
 
+}
 
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
