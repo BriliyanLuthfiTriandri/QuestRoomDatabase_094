0533d2cde7f9d203002deb7b9106fbe652df7f26
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-16 19:36:19 +0700

    Menambahkan proses untuk memuat dan menampilkan data mahasiswa di UpdateMhsViewModel berdasarkan NIM

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/UpdateMhsViewModel.kt
@@ -18,6 +18,20 @@ class UpdateMhsViewModel(
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel() {
 
+    var updateUIState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
+
 }
 
 fun Mahasiswa.toUIStateMhs() : MhsUIState = MhsUIState (
