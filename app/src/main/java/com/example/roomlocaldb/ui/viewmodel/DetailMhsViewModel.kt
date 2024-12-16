4086befac83d6c02b29aae041744f29e6b3f6d63
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-16 19:08:39 +0700

    Menambahkan fungsi ektensi untuk mengubah objek mahasiswa menjadi MahasiswaEvent

---
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.roomlocaldb.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomlocaldb.data.entity.Mahasiswa
+import com.example.roomlocaldb.repository.RepositoryMhs
+import com.example.roomlocaldb.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+fun Mahasiswa.toDetailUiEvent () : MahasiswaEvent{
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
