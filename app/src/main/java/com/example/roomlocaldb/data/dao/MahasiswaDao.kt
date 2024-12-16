e7c95c38eceb9bff1a7b142e8632ec95647d0ebf
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-12-16 14:31:17 +0700

    Menambahkan operasi CRUD di interface MahasiswaDao

--- app/src/main/java/com/example/roomlocaldb/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomlocaldb/data/dao/MahasiswaDao.kt
@@ -1,13 +1,33 @@
 package com.example.roomlocaldb.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface MahasiswaDao {
+interface MahasiswaDao{
     @Insert
     suspend fun insertMahasiswa(
         mahasiswa: Mahasiswa
     )
+    @Update
+    suspend fun updateMahasiswa(
+        mahasiswa: Mahasiswa
+    )
+    @Delete
+    suspend fun deleteMahasiswa(
+        mahasiswa: Mahasiswa
+    )
+    @Query
+        ("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+
+    @Query
+        ("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
 }
